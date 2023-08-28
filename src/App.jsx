e627d5ff3b984b35bc209c1ce1acbed69959a247
Wellington Marques <well06marques@gmail.com>
2023-08-27 21:27:19 -0300

    corrigindo rota

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         <Header />
         <div className='content'>
           <Routes>
-            <Route path="/" element={<Produtos />} />
+            <Route path="/router/" element={<Produtos />} />
             <Route path='produto/:id' element={<Produto />} />
             <Route path='contato' element={<Contato />} />
           </Routes>
